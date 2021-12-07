d73c2d1f243a78a616ef7f1425668d2efe3f6bc8
jamiguel23 <josematthewmiguel@gmail.com>
2021-12-06 19:55:18 -0800

    catch all error message

--- src/App.js
+++ src/App.js
@@ -9,13 +9,24 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state ={
-      queryCity: ''
+      queryCity: '',
+      locationObject: {},
+      error :false
     }
   }
 
   getLocation = async() => {
-    let result = await axios.get(`https://us1.locationiq.com/v1/search.php?key=pk.38e0d6bfcc9cadcc468246793fb2df34&q=${this.state.queryCity}&format=json`)
-    console.log(result.data);
+    try {
+
+      let result = await axios.get(`https://us1.locationiq.com/v1/search.php?key=pk.38e0d6bfcc9cadcc468246793fb2df34&q=${this.state.queryCity}&format=json`)
+      console.log(result.data[0]);
+      this.setState({locationObject: result.data[0]})
+
+    } catch(error){
+      console.log(error);
+      console.log('there was an error')
+      this.setState({error :true})
+    }
   }
 
   handleSubmit = (e) => {
@@ -32,6 +43,9 @@ export default class App extends Component {
         <button type="submit">Explore!</button>
 
         </form>
+
+        {this.state.locationObject.display_name? <p> {this.state.locationObject.display_name} </p> : <p> Search for a city to explore</p>}
+        {this.state.error && <p> Type in a city, there was an error in your input</p>};
       </div>
     )
   }
