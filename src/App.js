7d8e5940ecf26cedc79ac16526a90e17b84040fc
jamiguel23 <josematthewmiguel@gmail.com>
2021-12-06 16:33:33 -0800

    created props constructor to have the query city, created form with input and button

--- src/App.js
+++ src/App.js
@@ -2,10 +2,22 @@
 import React, { Component } from 'react'
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state ={
+      queryCity: ''
+    }
+  }
   render() {
     return (
       <div>
-        <h1>Hello! </h1>
+        <form onSubmit={this.handleSubmit}> 
+
+        <input type="text" placeholder="city name" name="city"></input>
+        <button type="submit">Find City</button>
+
+        </form>
       </div>
     )
   }
